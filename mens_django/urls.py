29810d51411acb9c2fc788e9be20d0301a3fd914
kunalpawar08essentia <kunal.pawar@essentia.dev>
2021-10-14 12:07:59 +0530

    Added Order app, added models, serializers and view for order details

--- mens_django/urls.py
+++ mens_django/urls.py
@@ -4,11 +4,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    
-    path('', admin.site.urls),
+
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/', include('product.urls')),
-] + static (settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
-
+    path('api/v1/', include('order.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
